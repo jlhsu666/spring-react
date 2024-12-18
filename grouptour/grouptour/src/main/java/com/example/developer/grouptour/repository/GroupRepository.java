488996877c8c35b69476dfdcc971434824b5f9da
Ron <srl@gapps.knu.edu.tw>
2024-12-18 22:44:45 +0800

    group tour finished

--- grouptour/grouptour/src/main/java/com/example/developer/grouptour/repository/GroupRepository.java
+++ grouptour/grouptour/src/main/java/com/example/developer/grouptour/repository/GroupRepository.java
@@ -1,10 +1,7 @@
 package com.example.developer.grouptour.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.developer.grouptour.model.Group;
-
-// import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Group findByName(String name);
